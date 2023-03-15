d05e191161791a28d462d6742102b9c925bf9690
Junio Dias <9145junio@gmail.com>
2023-03-15 11:50:26 -0300

    meu python

---
+++ soma.py
@@ -0,0 +1,7 @@
+numero1 = int (input("Digite um numero"))
+numero2 = int(input("Digite mais um numero"))
+
+resposta_soma = numero1 + numero2
+resposta_multiplica = numero1 * numero2
+
+print ("Asoma foi ", resposta_soma, "E a multiplicacao foi ", resposta_multiplica)
